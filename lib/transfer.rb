75e2c4a8a52c546607b834d908bcdadde4335712
Mokhtar Ali <joemokhtarali@gmail.com>
2020-01-13 02:23:15 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,27 @@
+require_relative './bank_account.rb'
+require "pry"
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    # binding.pry 
+    @sender.valid? && @receiver.valid? 
+  end
+
+  def execute_transaction
+    
+  end
+
+
 end
+
+
+# transfer = Transfer.new( Bank_account.new("mary"), Bank_account.new("meg")  25)
